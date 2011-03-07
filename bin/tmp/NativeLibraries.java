907ac7101e5171093ee6e8b3fc8b7fb8a040638a
Tim Jakobsson <tim.jakobsson@gmail.com>
2011-03-07 14:55:02 +0100

    added the newest version of the javascript

--- bin/tmp/NativeLibraries.java
+++
@@ -1,8 +0,0 @@
-package com.rhomobile.rhodes;
-public class NativeLibraries {
-  public static void load() {
-    // Load native .so libraries
-    // Load native implementation of rhodes
-    System.loadLibrary("rhodes");
-  }
-};
